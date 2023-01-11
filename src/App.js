7d3c98f68eccb70381fd25ea12223f674cdc0ed1
Singh Aditya Ravindra <50865286+Aditya2235@users.noreply.github.com>
2023-01-11 23:55:04 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import About from './components/About';
+import Card from './components/Card';
+import ContactMe from './components/Contact';
+import Footer from './components/Footer';
+import Skills from './components/Skills';
+function App() {
+  return (
+    <div className ='min-h-screen py-10 px-3 sm: px-5 bg-gray-100'>
+      <Card/>
+      <About/>
+      <Skills/>
+      <ContactMe/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
